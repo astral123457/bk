73e97937fecbd23fce5a35ca428bf767f8891204
Amauri Bueno Dos Santos <amauri_long@tutamail.com>
2025-03-08 20:08:12 -0300

    Add files via upload

--- src/main/java/com/guia/bk/Bk.java
+++ src/main/java/com/guia/bk/Bk.java
@@ -5,19 +5,12 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
-
 import org.bukkit.block.ShulkerBox;
-
 import org.bukkit.enchantments.Enchantment;
-
-
 import org.bukkit.inventory.meta.BlockStateMeta;
-
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -32,11 +25,30 @@ public final class Bk extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Mensagem no console para indicar ativação
         getLogger().info("Bk plugin habilitado!");
+
+        // Registrar eventos
         getServer().getPluginManager().registerEvents(this, this);
+
+        // Configurar banco de dados
         setupDatabase();
+
+        // Registrar comandos
+        getCommand("oi").setExecutor(this);
+        getCommand("vip").setExecutor(this);
+        getCommand("addvip").setExecutor(this);
+        getCommand("rmvip").setExecutor(this);
+
+        getCommand("emanuel").setExecutor(this);
+        getCommand("lilith").setExecutor(this);
+        getCommand("eva").setExecutor(this);
+        getCommand("adan").setExecutor(this);
+
+        getCommand("limpar").setExecutor(this);
+
     }
+
 
     @Override
     public void onDisable() {
@@ -51,6 +63,9 @@ public final class Bk extends JavaPlugin implements Listener {
     public void adicionarItem(Player player) {
         if (contador < 3) {
             player.getInventory().addItem(new ItemStack(Material.COOKED_BEEF, 1));
+            player.getInventory().addItem(new ItemStack(Material.BREAD, 1));
+            player.getInventory().addItem(new ItemStack(Material.APPLE, 1));
+
             contador++;
             player.sendMessage(ChatColor.GREEN + "Você recebeu um hambúrguer durante o dia de hoje! (" + contador + "/3)");
         } else {
@@ -70,21 +85,17 @@ public final class Bk extends JavaPlugin implements Listener {
                     sender.sendMessage(ChatColor.GREEN + "Você acaba de receber um kit de sobrevivência básico!");
 
                     // Dando o kit ao jogador
-                    player.getInventory().addItem(new ItemStack(Material.TORCH, 1));
-                    player.getInventory().addItem(new ItemStack(Material.IRON_PICKAXE, 1));
-                    player.getInventory().addItem(new ItemStack(Material.IRON_SWORD, 1));
+                    player.getInventory().addItem(new ItemStack(Material.TORCH, 13));
                     player.getInventory().addItem(new ItemStack(Material.BOOK, 1));
-                    player.getInventory().addItem(new ItemStack(Material.DIAMOND, 2));
-                    player.getInventory().addItem(new ItemStack(Material.OBSIDIAN, 3));
-                    player.getInventory().addItem(new ItemStack(Material.IRON_INGOT, 5));
-                    player.getInventory().addItem(new ItemStack(Material.EMERALD, 26));
+                    player.getInventory().addItem(new ItemStack(Material.BREAD, 3));
+                    player.getInventory().addItem(new ItemStack(Material.APPLE, 2));
+                    player.getInventory().addItem(new ItemStack(Material.CHERRY_WOOD, 36));
 
 
                     markKitReceived(playerName);
                 } else {
                     sender.sendMessage(ChatColor.LIGHT_PURPLE + "Bem vindo de volta, " + playerName + "!");
-                    sender.sendMessage(ChatColor.GREEN + "Você já recebeu seu kit. Aqui está um Livro como presente de retorno!");
-                    player.getInventory().addItem(new ItemStack(Material.BOOK, 1));
+                    sender.sendMessage(ChatColor.GREEN + "Você já recebeu seu kit!");
                     adicionarItem(player);
                 }
 
@@ -95,10 +106,58 @@ public final class Bk extends JavaPlugin implements Listener {
             }
         }
 
-        else if (command.getName().equalsIgnoreCase("Emanuel")) {
+        else if (command.getName().equalsIgnoreCase("addvip")) {
+            if (sender.hasPermission("vip.add")) {
+                if (args.length == 1) {
+                    String targetPlayerName = args[0];
+                    Player targetPlayer = getServer().getPlayer(targetPlayerName);
+
+                    // Executa o comando no console para adicionar VIP
+                    getServer().dispatchCommand(getServer().getConsoleSender(),
+                            "lp user " + targetPlayerName + " permission set vip.use true");
+
+                    if (targetPlayer != null) {
+                        addVipToDatabase(targetPlayerName);
+                        sender.sendMessage(ChatColor.GREEN + "O jogador " + targetPlayerName + " agora é um VIP!");
+                        targetPlayer.sendMessage(ChatColor.GOLD + "Parabéns, você recebeu o status de VIP!");
+                    } else {
+                        sender.sendMessage(ChatColor.RED + "Jogador não encontrado.");
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Uso correto: /addvip <nome_do_jogador>");
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "Você não tem permissão para usar este comando.");
+            }
+            return true;
+        }
+
+        else if (command.getName().equalsIgnoreCase("rmvip")) {
+            if (sender.hasPermission("vip.add")) {
+                if (args.length == 1) {
+                    String targetPlayerName = args[0];
+                    if (removeVipFromDatabase(targetPlayerName)) {
+                        // Executa o comando no console para remover VIP
+                        getServer().dispatchCommand(getServer().getConsoleSender(),
+                                "lp user " + targetPlayerName + " permission unset vip.use");
+
+                        sender.sendMessage(ChatColor.GREEN + "O jogador " + targetPlayerName + " não é mais VIP.");
+                    } else {
+                        sender.sendMessage(ChatColor.RED + "O jogador " + targetPlayerName + " não foi encontrado como VIP.");
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Uso correto: /rmvip <nome_do_jogador>");
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "Você não tem permissão para usar este comando.");
+            }
+            return true;
+        }
+
+        else if (command.getName().equalsIgnoreCase("emanuel")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (player.hasPermission("Emanuel.use")) {
+                if (player.hasPermission("vip.use")) {
                     sender.sendMessage(ChatColor.GOLD + "XAΓ, Cristo  estratégia de precificação, escala, série de ideias,  uma estrela, um macho dominante!");
                     // Adicionando itens VIP ao jogador
 
@@ -109,7 +168,6 @@ public final class Bk extends JavaPlugin implements Listener {
                     player.getInventory().addItem(new ItemStack(Material.TOTEM_OF_UNDYING, 1));//Sepulcral
                     player.getInventory().addItem(new ItemStack(Material.COAL, 40));//Covodos
                     player.getInventory().addItem(new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 2));
-                    player.getInventory().addItem(new ItemStack(Material.WATER_BUCKET, 42)); // 42 Baldes de Água
                     player.getInventory().addItem(new ItemStack(Material.GOLD_INGOT, 5)); // + 5 GOLD LINGOT
 
 
@@ -174,10 +232,10 @@ public final class Bk extends JavaPlugin implements Listener {
             }
         }
 
-        else if (command.getName().equalsIgnoreCase("Lilith")) {
+        else if (command.getName().equalsIgnoreCase("lilith")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (player.hasPermission("Lilith.use")) {
+                if (player.hasPermission("vip.use")) {
                     sender.sendMessage(ChatColor.GOLD + "ΘΕ, Consciência emocional é pequena e leva a Morte Princesa Lilith");
                     // Adicionando itens VIP ao jogador
 
@@ -210,10 +268,10 @@ public final class Bk extends JavaPlugin implements Listener {
                 return true;
             }
         }
-        else if (command.getName().equalsIgnoreCase("Eva")) {
+        else if (command.getName().equalsIgnoreCase("eva")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (player.hasPermission("Eva.use")) {
+                if (player.hasPermission("vip.use")) {
                     sender.sendMessage(ChatColor.GOLD + "ΣΚ, Lesbica Feminista é uma criatura aquática, semelhante a uma tartaruga, que habita rios e lagos ( EVA e Seus pecados Do mundo)");
                     // Adicionando itens VIP ao jogador
                     player.getInventory().addItem(new ItemStack(Material.DIAMOND, 64));// Rainha do Mar do Lado Oeste 27 Talentos
@@ -233,7 +291,7 @@ public final class Bk extends JavaPlugin implements Listener {
                     // Criar um escudo
                     ItemStack shield = new ItemStack(Material.SHIELD, 1);
 
-                    // Adicionar encantamentos ao escud
+                    // Adicionar encantamentos ao escudo
                     shield.addUnsafeEnchantment(Enchantment.getByName("DURABILITY"), 3); // Inquebrável III
                     shield.addUnsafeEnchantment(Enchantment.MENDING, 1); // Remendo
 
@@ -251,19 +309,33 @@ public final class Bk extends JavaPlugin implements Listener {
                 return true;
             }
         }
-        else if (command.getName().equalsIgnoreCase("Adan")) {
+        else if (command.getName().equalsIgnoreCase("adan")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (player.hasPermission("Adan.use")) {
+                if (player.hasPermission("vip.use")) {
                     sender.sendMessage(ChatColor.GOLD + "ΔΙ, LAtino Pequeno (Homen Barro Adao)");
                     // Adicionando itens VIP ao jogador
                     player.getInventory().addItem(new ItemStack(Material.COBBLESTONE, 64)); // 1 Esporao de 64 Rochas (Pedregulho)
                     player.getInventory().addItem(new ItemStack(Material.IRON_INGOT, 6));// Prata
-                    player.getInventory().addItem(new ItemStack(Material.WATER_BUCKET, 22)); // 22 Baldes de Água
+
                     player.getInventory().addItem(new ItemStack(Material.IRON_INGOT, 40));// Prata
                     player.getInventory().addItem(new ItemStack(Material.TNT, 13)); // 13 blocos de TNT
                     player.getInventory().addItem(new ItemStack(Material.SHEEP_SPAWN_EGG, 14)); // 14 Talentos do Norte do Friu bicho peludinho ovos de ovelhas
 
+                    // Criar a Shulker Box cheia de diamantes
+                    ItemStack shulkerBox = new ItemStack(Material.SHULKER_BOX);
+                    BlockStateMeta meta = (BlockStateMeta) shulkerBox.getItemMeta();
+                    ShulkerBox shulker = (ShulkerBox) meta.getBlockState();
+
+                    player.getInventory().addItem(new ItemStack(Material.WATER_BUCKET, 22)); // 22 Baldes de Água
+
+
+                    // Salvar e aplicar as mudanças na Shulker Box
+                    meta.setBlockState(shulker);
+                    shulkerBox.setItemMeta(meta);
+
+                    // Adicionar a Shulker Box ao inventário do jogador
+                    player.getInventory().addItem(shulkerBox);
 
 
                 } else {
@@ -276,18 +348,31 @@ public final class Bk extends JavaPlugin implements Listener {
             }
         }
 
+        else if (command.getName().equalsIgnoreCase("limpar")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (player.hasPermission("vip.use")) {
+
+                        // Remove todos os itens do inventário do jogador
+                        player.getInventory().clear();
+                        player.sendMessage(ChatColor.GREEN + "Todo o seu inventário foi limpo!");
+
+                    } else {
+                        sender.sendMessage("Este comando só pode ser executado por um jogador.");
+                    }
+                return true;
+            } else {
+                sender.sendMessage("Este comando só pode ser executado por um jogador.");
+                return true;
+            }
+        }
+
+
         else if (command.getName().equalsIgnoreCase("vip")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
                 if (player.hasPermission("vip.use")) {
                     sender.sendMessage(ChatColor.GOLD + "Bem-vindo, VIP!");
-                    // Adicionando itens VIP ao jogador
-                    player.getInventory().addItem(new ItemStack(Material.DIAMOND, 64));
-                    player.getInventory().addItem(new ItemStack(Material.IRON_INGOT, 64));
-                    player.getInventory().addItem(new ItemStack(Material.GOLD_INGOT, 64));
-                    player.getInventory().addItem(new ItemStack(Material.NETHERITE_BLOCK, 64));
-                    player.getInventory().addItem(new ItemStack(Material.EMERALD, 64));
-                    player.getInventory().addItem(new ItemStack(Material.COAL, 64));
                     player.getInventory().addItem(new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 2));
                 } else {
                     sender.sendMessage(ChatColor.RED + "Você não tem permissão para usar este comando.");
@@ -300,6 +385,8 @@ public final class Bk extends JavaPlugin implements Listener {
         }
         return false;
     }
+
+
 
     private void setupDatabase() {
         try (Connection conn = DriverManager.getConnection(DB_URL); Statement stmt = conn.createStatement()) {
@@ -318,11 +405,51 @@ public final class Bk extends JavaPlugin implements Listener {
                     "pitch FLOAT NOT NULL," +
                     "PRIMARY KEY (player_id))";
             stmt.executeUpdate(sql2);
+
+
+            String sql3 = "CREATE TABLE IF NOT EXISTS vip_players (" +
+                    "player_name VARCHAR(255) NOT NULL," +
+                    "PRIMARY KEY (player_name))";
+            stmt.executeUpdate(sql3);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+// Métodos para armazenar e excluir
+    private void addVipToDatabase(String playerName) {
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO vip_players (player_name) VALUES (?)")) {
+            pstmt.setString(1, playerName);
+            pstmt.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private boolean removeVipFromDatabase(String playerName) {
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement("DELETE FROM vip_players WHERE player_name = ?")) {
+            pstmt.setString(1, playerName);
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean isVip(String playerName) {
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement("SELECT player_name FROM vip_players WHERE player_name = ?")) {
+            pstmt.setString(1, playerName);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 
 
